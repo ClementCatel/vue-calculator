45d23bc24e9639bc471e9564420f6aa9aec1bb5e
Clément Catel <clement.catel4@gmail.com>
2019-02-20 16:44:02 +0100

    Add operations logic (operation priority not handled) + clear function

--- src/components/Calculator.vue
+++ src/components/Calculator.vue
@@ -1,42 +1,127 @@
 <template>
     <div class="calculator">
-      <div class="display">2+2</div>
-      <div class="display result">4</div>
+      <div class="display" >{{ current || '0' }}</div>
+      <div class="display result">{{ result }}</div>
 
-      <div class="btn mem">mc</div>
-      <div class="btn mem">m+</div>
-      <div class="btn mem">m-</div>
-      <div class="btn mem">mr</div>
+      <div class="btn mem" tabindex="1">mc</div>
+      <div class="btn mem" tabindex="2">m+</div>
+      <div class="btn mem" tabindex="3">m-</div>
+      <div class="btn mem" tabindex="4">mr</div>
 
-      <div class="btn operator">C</div>
-      <div class="btn operator fas fa-divide"></div>
-      <div class="btn operator fas fa-times"></div>
-      <div class="btn operator fas fa-backspace"></div>
+      <div @click="clear" class="btn operator" tabindex="5">C</div>
+      <div @click="divide" class="btn operator fas fa-divide" tabindex="6"></div>
+      <div @click="times" class="btn operator fas fa-times" tabindex="7"></div>
+      <div @click="backspace" class="btn operator fas fa-backspace" tabindex="8"></div>
 
-      <div class="btn">7</div>
-      <div class="btn">8</div>
-      <div class="btn">9</div>
-      <div class="btn operator fas fa-minus"></div>
+      <div @click="append('7')" class="btn" tabindex="9">7</div>
+      <div @click="append('8')" class="btn" tabindex="10">8</div>
+      <div @click="append('9')" class="btn" tabindex="11">9</div>
+      <div @click="minus" class="btn operator fas fa-minus" tabindex="12"></div>
 
-      <div class="btn">4</div>
-      <div class="btn">5</div>
-      <div class="btn">6</div>
-      <div class="btn operator fas fa-plus"></div>
+      <div @click="append('4')" class="btn" tabindex="13">4</div>
+      <div @click="append('5')" class="btn" tabindex="14">5</div>
+      <div @click="append('6')" class="btn" tabindex="15">6</div>
+      <div @click="add" class="btn operator fas fa-plus" tabindex="16"></div>
 
-      <div class="btn">1</div>
-      <div class="btn">2</div>
-      <div class="btn">3</div>
-      <div class="btn equals fas fa-equals"></div>
+      <div @click="append('1')" class="btn" tabindex="17">1</div>
+      <div @click="append('2')" class="btn" tabindex="18">2</div>
+      <div @click="append('3')" class="btn" tabindex="19">3</div>
+      <div @click="equals" class="btn equals fas fa-equals" tabindex="20"></div>
 
-      <div class="btn">%</div>
-      <div class="btn">0</div>
-      <div class="btn">.</div>
+      <div @click="percent" class="btn" tabindex="21">%</div>
+      <div @click="append('0')" class="btn" tabindex="22">0</div>
+      <div @click="dot" class="btn" tabindex="23">.</div>
     </div>
 </template>
 
 <script>
 export default {
-  
+  data() {
+    return {
+      previous: null,
+      current: '',
+      currentValue: '',
+      result: '',
+      operator: null,
+      operatorClicked: false,
+    }
+  },
+  methods: {
+    clear() {
+      this.current = this.result = this.currentValue = '';
+      this.operator = this.previous = null;
+    },
+
+    backspace() {
+      this.current = this.current.slice(0, this.current.length - 1);
+      this.compute();
+      this.operatorClicked = false;
+    },
+
+    compute() {
+      if (this.operator != null) {
+        this.result = `${this.operator(parseFloat(this.previous), parseFloat(this.currentValue))}`;
+      }
+    },
+
+    // TODO : recalculate the percentage every time it is clicked
+    percent() {
+      this.current = `${this.current}${'%'}`;
+      this.result = `${parseFloat(this.current) / 100}`;
+    },
+ 
+    append(number) {
+      this.current = `${this.current}${number}`;
+      this.currentValue = `${this.currentValue}${number}`;
+      this.compute();
+      this.operatorClicked = false;
+    },
+
+    appendOperator(operator) {
+      if (this.current !== '' && !this.operatorClicked) {
+        this.current = `${this.current}${operator}`;
+      }
+    },
+
+    dot() {
+      if (!this.current.includes('.')) {
+        this.current = `${this.current}${'.'}`
+      }  
+    },
+
+    setPrevious() {
+      this.result === '' ?
+        this.previous = this.currentValue : this.previous = this.result;
+      this.currentValue = '';
+      this.operatorClicked = true;
+    },
+
+    divide() {
+      this.operator = (a, b) => a / b;
+      this.appendOperator('÷')
+      this.setPrevious();
+    },
+    times() {
+      this.operator = (a, b) => a * b;
+      this.appendOperator('×')
+      this.setPrevious();
+    },
+    minus() {
+      this.operator = (a, b) => a - b;
+      this.appendOperator('−')
+      this.setPrevious();
+    },
+    add() {
+      this.operator = (a, b) => a + b;
+      this.appendOperator('+')
+      this.setPrevious();
+    },
+
+    equals() {
+      this.current = this.currentValue = this.result;
+      this.result = '';
+    }
+  },
 }
 </script>
 
@@ -49,7 +134,7 @@ export default {
     display: grid;
     grid-template-columns: repeat(4, 1fr);
     grid-auto-rows: minmax(100px, auto);
-    background-color: rgb(251, 251, 251);
+    background-color: white;
   }
 
   .display {
@@ -84,6 +169,10 @@ export default {
     display: flex;
     align-items: center;
     justify-content: center; 
+  }
+
+  .btn :focus {
+    background-color: #aaa
   }
 
   .operator {
